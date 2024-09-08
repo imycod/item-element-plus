631e63db4dbd148ad713977f82f552337e858b4f
Jay Wu <jay.wu@item.com>
2024-09-08 22:11:55 +0800

    feat：App.vue demo

---
+++ play/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <i-button></i-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { IButton } from '@unisc-ui/item';
+import { IButton } from 'item-ui/es';
+</script>
